6ea3dfc3912db6ac2989b898c05da6605c0db860
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-19 15:55:29 -0200

    Linked First App and index

--- kingpin/urls.py
+++ kingpin/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include('first_app.urls')),
+    url(r'^first_app/', include('first_app.urls'))    
 ]
