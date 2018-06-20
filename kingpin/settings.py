5bf7930ab09e85651ea9e3f3a1936aa0651d8cb0
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-06-20 08:50:55 -0200

    Configured Static Files

--- kingpin/settings.py
+++ kingpin/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #os.path.join() OS optimised path joiner
 
 TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
+STATIC_DIR = os.path.join(BASE_DIR,"static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# see documentation above very good!
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
